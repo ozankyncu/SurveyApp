a8d027b395ca966eb0e54e1095b712f5237ed005
Ozan Koyuncu <ozankyncu@gmail.com>
2020-01-15 23:04:54 +0300

    staging conf added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
-if (process.env.NODE_ENV === 'production') {
+if (
+  process.env.NODE_ENV === 'production' ||
+  process.env.NODE_ENV === 'staging'
+) {
   module.exports = require('./prod');
 } else {
   module.exports = require('./dev');
